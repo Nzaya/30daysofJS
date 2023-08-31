646bb8c49ae79eed1abd4e2f85ce1dd9e489a985
sharon nzaya <nzayangeta4149@gmail.com>
2023-08-31 16:36:25 +0300

    day 2

--- Data Types/day2.js
+++ Data Types/day2.js
@@ -441,53 +441,107 @@ console.log(days.startsWith(30));
 
 // Use endsWith() method with the string 30 Days Of JavaScript and make the result true
 console.log(days.endsWith('JavaScript'));
+
 // Use match() method to find all the a’s in 30 Days Of JavaScript
 console.log(days.match('a'))
+
 // Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
 let jav = '30 Days of'
 console.log(jav.concat('JavaScript'));
+
 // Use repeat() method to print 30 Days Of JavaScript 2 times
 console.log(days.repeat(2));
 
 
 // Exercise: Level 2
 // Using console.log() print out the following statement:
-
 // The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
-// Using console.log() print out the following quote by Mother Teresa:
+console.log('There is no exercise better for the heart than reaching down and lifting people up. by John Holmes teaches us to help one another.');
 
+// Using console.log() print out the following quote by Mother Teresa:
 // "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.");
+
 // Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log(typeof '10' == 10);
+
+num = '10'
+numInt = parseInt(10)
+console.log(numInt);
+console.log(typeof numInt === typeof 10);
 
 // Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+numm = '9.8'
+nummFloat = parseFloat(numm)
+console.log(nummFloat);
+console.log(typeof nummFloat == typeof 10);
 
 // Check if 'on' is found in both python and jargon
+word = 'python and jargon'
+console.log(word.match('on'));
 
 // I hope this course is not full of jargon. Check if jargon is in the sentence.
+sent = ' I hope this course is not full of jargon.'
+console.log(sent.match('jargon'));
 
 // Generate a random number between 0 and 100 inclusively.
+let N = 100
+nuum = Math.random() * N
+console.log(nuum);
 
 // Generate a random number between 50 and 100 inclusively.
+const min = 50;
+const max = 100;
+
+a = Math.floor(Math.random() * (max - min + 1) + min)
+console.log(`Random number between ${min} and ${max} is ${a}`);
 
 // Generate a random number between 0 and 255 inclusively.
 
+let P = 255
+rand = Math.floor(Math.random() * P)
+console.log(rand);
+
 // Access the 'JavaScript' string characters using a random number.
+const text = 'JavaScript'
+const result = Math.floor(Math.random() * text.length)
+randChar = text[result]
+console.log(`Random Index = ${result}`);
+console.log(`Random Character = ${randChar}`);
+
 
 // Use console.log() and escape characters to print the following pattern.
-
 // 1 1 1 1 1
 // 2 1 2 4 8
 // 3 1 3 9 27
 // 4 1 4 16 64
 // 5 1 5 25 125
+
+console.log( "1  1  1  1 \n  2  1  2  4  8 \n  3  1  3  9  27 \n 4  1  4  16  64 \n 5  1  5  25  125");
+
 // Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+sent = 'You cannot end a sentence with because because because is a conjunction'
+console.log(sent.substr(sent.indexOf('because'), ('because'.length * 3) + 2));
 
 // Exercises: Level 3
 // 'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+pattern = /love/gi
+console.log(love.match(pattern));
 
 // Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+all = 'You cannot end a sentence with because because because is a conjunction'
+pat = /because/gi
+console.log(all.match(pat));
 
-// Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+// Clean the following text and find the most frequent word (hint, use replace and regular expressions)'
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
-//     const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+cleaned = sentence.replace(/[^\w\s]/g, '').replace(/\s+/g, ' ');
+console.log(cleaned);
+
+
 // Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+income = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+regEx = /\d+/g
+console.log(income.match(regEx));

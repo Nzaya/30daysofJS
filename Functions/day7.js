d92dd04f432ca9e7fab53aaa3aa987f5f83ab883
sharon nzaya <nzayangeta4149@gmail.com>
2023-10-09 18:07:32 +0300

    first

--- Functions/day7.js
+++ Functions/day7.js
@@ -191,46 +191,169 @@ console.log('Weight of an object in Newton:', weightOfObject(200));
 //! 💻 Exercises
 //? Exercises: Level 1
 // Declare a function fullName and it print out your full name.
+function fullNames(firstName, lastName){
+    return `${firstName} ${lastName}`
+}
+console.log(fullNames("Mary", "Jane"));
+
+function fullname(){
+    let firstName = 'John'
+    let lastName = 'Jane'
+    let space = ' '
+    const name = firstName + space + lastName
+    return name
+}
+console.log(fullname());
 
 // Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+function fullnames (firstName, lastName){
+    return `${firstName}, ${lastName}`
+}
+console.log(fullnames('Mercy', 'Abel'));
 
 // Declare a function addNumbers and it takes two two parameters and it returns sum.
+function addnumbers(num1, num2){
+    sum = num1 + num2
+    return sum
+}
+console.log(addnumbers(10,12));
 
 // An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+function areaOfRectangle(length, width){
+    area = length * width
+    return area
+}
+console.log(areaOfRectangle(12,2));
 
 // A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+const perimeterOfRectangle = (length, width) =>{
+    perimeter = 2*(length + width)
+    return perimeter
+}
+console.log(perimeterOfRectangle(12,2));
 
 // A volume of a rectangular prism is calculated as follows: volume = length x width x height. Write a function which calculates volumeOfRectPrism.
+function volumeOfRectangle (length, width, height){
+    volume = length * width * height
+    return volume
+}
+console.log(volumeOfRectangle(2,4,6));
 
 // Area of a circle is calculated as follows: area = π x r x r. Write a function which calculates areaOfCircle
+const areaOfCircles = (r) => {
+    π = 22/7 
+    area = π * r * r
+    return area
+}
+console.log(areaOfCircles(7));
 
 // Circumference of a circle is calculated as follows: circumference = 2πr. Write a function which calculates circumOfCircle
+function circumOfCircle (r){
+    π = 22/7 
+    circumfrence = 2 * π * r
+    return circumfrence
+}
+console.log(circumOfCircle(7));
 
 // Density of a substance is calculated as follows:density= mass/volume. Write a function which calculates density.
+const densityOfSubstance = (mass, volume) => {
+    density = mass / volume
+    return density
+}
+console.log(densityOfSubstance(10,4));
 
 // Speed is calculated by dividing the total distance covered by a moving object divided by the total amount of time taken. Write a function which calculates a speed of a moving object, speed.
+const calcSpeed = (distance, time) =>{
+    speed = distance / time
+    return speed
+}
+console.log(calcSpeed(240, 4));
 
 // Weight of a substance is calculated as follows: weight = mass x gravity. Write a function which calculates weight.
+function calcWeight(mass, gravity = 9.81){
+    weight = mass * gravity + 'N'
+    return weight
+}
+console.log(calcWeight(12));
 
 // Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
+function convertCelsiusToFahrenheit(oC){
+    oF = (oC * 9/5) + 32
+    return oF
+}
+console.log(convertCelsiusToFahrenheit(24));
 
-// Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+
+// Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi.
+//  BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 // The same groups apply to both men and women.
 // Underweight: BMI is less than 18.5
 // Normal weight: BMI is 18.5 to 24.9
 // Overweight: BMI is 25 to 29.9
 // Obese: BMI is 30 or more
 
-
+function calcBMI(weight, height){
+    bmi = weight / (height * height)
+    if(bmi < 18.5){
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi <=24.9){
+        return 'Normal Weight';
+    }else if (bmi >=25 && bmi <=29.9){
+        return 'Overweight';
+    } else{
+        return 'Obese'
+    }
+}
+console.log(calcBMI(70, 1.35));
 
 // Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
+// September, October or November, the season is Autumn.
+// December, January or February, the season is Winter.
+// March, April or May, the season is Spring
+// June, July or August, the season is Summer
+
+function checkSeason(){
+    let userInput = 'January';
+    let month = userInput.toLowerCase()
+
+    switch (month){
+        case 'september': 
+        case 'october':
+        case 'november':
+            return 'The season is Autumn'
+            break;
+        case 'december':
+        case 'january':
+        case 'february':
+            return 'The season is Winter'
+            break;
+        case 'march':
+        case 'april':
+        case 'may':
+            return 'The season is spring'
+            break;
+        case 'june':
+        case 'july':
+        case 'august':
+            return 'The season is Summer'
+            break;
+        default:
+            return 'Input month'
+    }
+}
+console.log(checkSeason());
 
 // Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
 // console.log(findMax(0, 10, 5))
 // 10
 // console.log(findMax(0, -10, -2))
 // 0
-
+function largest(num1, num2, num3){
+    largestNum = Math.max(num1, num2, num3)
+    return largestNum
+}
+console.log(largest(0,10,5));
+console.log(largest(0,-10,-2));
 
 //? Exercises: Level 2
 // Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
